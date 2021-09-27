f5074a908e00ac8144fcd1cb48d1484522b8cc97
Robert Herber <rwherber@gmail.com>
2021-09-26 21:18:42 -0700

    chore: update packages

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const ignored = ['<rootDir>/dist/', '<rootDir>/src/__benchmarks__/'];
+
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ignored,
+  coveragePathIgnorePatterns: ignored,
+  modulePathIgnorePatterns: ignored,
+  watchPathIgnorePatterns: ignored
 };
